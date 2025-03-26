ce9b304b1a2475c30d0fa2b29c7519838a7de4df
Sautron Mattéo <146347981+SqualFost@users.noreply.github.com>
2025-03-26 12:58:25 +0100

    erreur de ma part, build impossible

--- app/api/user/profile/route.ts
+++ app/api/user/profile/route.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
-export const dynamic = "force-dynamic";
-
 const SECRET_KEY = process.env.JWT_SECRET || "supersecret";
 
 export async function GET() {
